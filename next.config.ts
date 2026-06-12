e34817210bd17de40f04171781212cf65a19ad06
souravmaji1 <majisourav255@gmail.com>
2026-06-12 12:58:03 +0530

    updated origin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  allowedDevOrigins: ['82.22.32.13'],
+}
 
-export default nextConfig;
+export default nextConfig
